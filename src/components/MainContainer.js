0cbf53be4443303a02ab5c88f145342fbf71deaf
Brian <mumobrian2746@gmail.com>
2024-11-13 11:12:07 +0300

    completed lab

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,18 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import StockContainer from "./StockContainer";
 import PortfolioContainer from "./PortfolioContainer";
 import SearchBar from "./SearchBar";
 
 function MainContainer() {
+  const [stocks, setStocks] = useState([]);
+  const [portfolio, setPortfolio] = useState([]);
+  const [sortBy, setSortBy] = useState("Alphabetically");
+  const [filter, setFilter] = useState("All");
+
+  useEffect(() => {
+    fetch("http://localhost:3001/stocks")
+      .then((res) => res.json())
+      .then((data) => setStocks(data));
+  }, []);
+
+  const addStockToPortfolio = (stock) => {
+    if (!portfolio.includes(stock)) setPortfolio([...portfolio, stock]);
+  };
+
+  const removeStockFromPortfolio = (stock) => {
+    setPortfolio(portfolio.filter((s) => s.id !== stock.id));
+  };
+
+  const sortedStocks = [...stocks]
+    .filter((stock) => filter === "All" || stock.type === filter)
+    .sort((a, b) =>
+      sortBy === "Alphabetically"
+        ? a.name.localeCompare(b.name)
+        : a.price - b.price
+    );
+
   return (
     <div>
-      <SearchBar />
+      <SearchBar setSortBy={setSortBy} setFilter={setFilter} />
       <div className="row">
         <div className="col-8">
-          <StockContainer />
+          <StockContainer stocks={sortedStocks} onStockClick={addStockToPortfolio} />
         </div>
         <div className="col-4">
-          <PortfolioContainer />
+          <PortfolioContainer portfolio={portfolio} onStockClick={removeStockFromPortfolio} />
         </div>
       </div>
     </div>
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,33 +1,25 @@
 import React from "react";
 
-function SearchBar() {
+function SearchBar({ setSortBy, setFilter }) {
+  const handleSortChange = (e) => setSortBy(e.target.value);
+  const handleFilterChange = (e) => setFilter(e.target.value);
+
   return (
     <div>
       <strong>Sort by:</strong>
       <label>
-        <input
-          type="radio"
-          value="Alphabetically"
-          name="sort"
-          checked={null}
-          onChange={null}
-        />
+        <input type="radio" value="Alphabetically" name="sort" onChange={handleSortChange} />
         Alphabetically
       </label>
       <label>
-        <input
-          type="radio"
-          value="Price"
-          name="sort"
-          checked={null}
-          onChange={null}
-        />
+        <input type="radio" value="Price" name="sort" onChange={handleSortChange} />
         Price
       </label>
       <br />
       <label>
         <strong>Filter:</strong>
-        <select onChange={null}>
+        <select onChange={handleFilterChange}>
+          <option value="All">All</option>
           <option value="Tech">Tech</option>
           <option value="Sportswear">Sportswear</option>
           <option value="Finance">Finance</option>
